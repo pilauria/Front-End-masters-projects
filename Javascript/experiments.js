915aec8a3ef0d7bd396dfaa66b0c82a30afa55dd
Pietro Lauria <ptr.lauria@gmail.com>
2021-05-04 09:11:22 +0200

    first commit

---
+++ Javascript/experiments.js
@@ -0,0 +1,13 @@
+const monthlyRent = 500;
+
+const yearlyRent = monthlyRent * 12;
+console.log(yearlyRent);
+
+
+let friendsAtYourParty = 0;
+while (friendsAtYourParty < 10) {
+  friendsAtYourParty = friendsAtYourParty + 1;
+  console.log(friendsAtYourParty);
+}
+console.log(friendsAtYourParty);
+
